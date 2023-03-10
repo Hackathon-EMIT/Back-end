ee74cef347184f83056b1c25348caf13156c3855
Anthony <hulkratah@gmail.com>
2023-03-10 23:56:15 +0300

    fix

--- src/SetupData/__setup.js
+++ src/SetupData/__setup.js
@@ -23,7 +23,6 @@ const {
         alter:true,
         force:true
     })
-<<<<<<< HEAD
     await setupData({ uri:__dirname+"/Achat.json", model: Achat });
     await setupData({ uri:__dirname+"/Client.json", model: Client });
     await setupData({ uri:__dirname+"/Facture.json", model: Facture });
@@ -34,16 +33,6 @@ const {
     await setupData({ uri:__dirname+"/Produit.json", model: Produit });
     await setupData({ uri:__dirname+"/Promo.json", model: Promo });
     await setupData({ uri:__dirname+"/Stock.json", model: Stock });
-
-
-
-
-=======
-    // await setupData({ uri:__dirname+"/Model.json", model: Model });
-    await setupData({ uri:__dirname+"/Fournisor.json", model: Fournisor });
-    await setupData({ uri:__dirname+"/Notif.json", model: Notif });
-    await setupData({ uri:__dirname+"/Facture.json", model: Facture });
->>>>>>> 851bd3eccc364662295f743d3f8c2a3eb094123d
 
 
     //await relationEntreModel(

ed470715994aba09b8e4a638f4ae827499a34fd7
Hajandrainy Leonardo <hajandrainynalyh@gmail.com>
2023-03-10 20:53:47 +0000

    add Fournisor

---
+++ src/Models/Fournisor.js
@@ -0,0 +1,30 @@
+const sequelize = require('./__sequelize');
+const {DataTypes,Model} = require("sequelize");
+
+class Fournisor extends Model {};
+fournisor.init({
+    four_id : {
+        type : DataTypes.STRING,
+        primaryKey:true,
+        defaultValue:DataTypes.UUIDV4,
+    },
+
+    adrs_four:{
+        type:DataTypes.STRING
+    },
+
+    name_four:{
+        type:DataTypes.STRING,
+    },
+
+    tel_four:{
+        type:DataTypes.STRING(10),
+    },
+
+    four_email:{
+        type:DataTypes.STRING,
+    },
+
+}, { sequelize })
+
+module.exports = Fournisor;

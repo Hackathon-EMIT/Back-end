629937ac0188712a5be1d6e02972128fe74ba1f0
ajl420 <ajl35353535@gmail.com>
2023-03-11 00:24:07 +0300

    Fix/SetupData

--- src/SetupData/__setup.js
+++ src/SetupData/__setup.js
@@ -2,7 +2,6 @@ const setupData = require("#Utils/setupData");
 
 const {
     //Model
-    Achat , 
     Client,
     Facture,
     Fournisor , 
@@ -24,7 +23,6 @@ const {
         force:true
     })
 
-    await setupData({ uri:__dirname+"/Achat.json", model: Achat });
     await setupData({ uri:__dirname+"/Client.json", model: Client });
     await setupData({ uri:__dirname+"/Facture.json", model: Facture });
     await setupData({ uri:__dirname+"/Fournisor.json", model: Fournisor });

30888d965509fae0755e2cef8bd2b8c13fdda3d1
Anthony <hulkratah@gmail.com>
2023-03-10 23:53:46 +0300

    fix

--- src/SetupData/__setup.js
+++ src/SetupData/__setup.js
@@ -4,7 +4,16 @@ const {
     //Model
     Achat , 
     Client,
-    
+    Facture,
+    Fournisor , 
+    Gerant , 
+    Notif , 
+    PointSale , 
+    Produit , 
+    Promo , 
+    Stock,
+
+
     sequelize
 } = require("#Model");
 
@@ -13,10 +22,24 @@ const {
 (async () => {
 
     await sequelize.sync({
-        // alter:true,
+        alter:true,
         force:true
     })
-    // await setupData({ uri:__dirname+"/Model.json", model: Model });
+    await setupData({ uri:__dirname+"/Achat.json", model: Achat });
+    await setupData({ uri:__dirname+"/Client.json", model: Client });
+    await setupData({ uri:__dirname+"/Facture.json", model: Facture });
+    await setupData({ uri:__dirname+"/Fournisor.json", model: Fournisor });
+    await setupData({ uri:__dirname+"/Gerant.json", model: Gerant });
+    await setupData({ uri:__dirname+"/Notif.json", model: Notif });
+    await setupData({ uri:__dirname+"/PointSale.json", model: PointSale });
+    await setupData({ uri:__dirname+"/Produit.json", model: Produit });
+    await setupData({ uri:__dirname+"/Promo.json", model: Promo });
+    await setupData({ uri:__dirname+"/Stock.json", model: Stock });
+
+
+
+
+
 
     //await relationEntreModel(
        // id,

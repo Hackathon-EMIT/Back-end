8b2f3ff9b5f55a0d5861f90ddda7ba0ff7943c11
ajl420 <ajl35353535@gmail.com>
2023-03-10 23:41:54 +0300

    Relation/Product and Fournisor

--- src/Models/index.js
+++ src/Models/index.js
@@ -3,9 +3,13 @@ const Produit = require("./Produit");
 const Achat = require('./Achat');
 const Gerant = require('./Gerant');
 const PointSale = require('./PointSale');
+const Fournisor = require("./Fournisor");
 const Promo = require('./Promo');
 const Stock = require("./Stock");
 const sequelize = require("./__sequelize");
+
+Produit.hasOne(Fournisor);
+Fournisor.hasMany(Produit);
 
 Produit.belongsToMany(PointSale, { through: Stock });
 PointSale.belongsToMany(Produit, { through: Stock });
@@ -16,6 +20,7 @@ Promo.belongsTo(Produit);
 Gerant.hasMany(PointSale);
 PointSale.belongsTo(Gerant);
 
+exports.Fournisor = Fournisor;
 exports.Achat = Achat;
 exports.Client = Client;
 exports.Produit = Produit;
